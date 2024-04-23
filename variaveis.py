d753d1857c5b5620fa843e19d0e04708d6883683
caduzin87 <carlos.bafonifacincani@gmail.com>
2024-04-23 16:37:34 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Carlos Eduardo"
+idade=16
+email='carlos.bafonifacincani@gmail.com'
+telefone='17 99223-3232'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
