94969a809a50c696ab99469b5186789e2ce9d5db
caduzin87 <carlos.bafonifacincani@gmail.com>
2024-05-21 14:58:04 -0300

    Update lista.py

--- listas.py
+++ listas.py
@@ -1,7 +1,7 @@
 aluno = ["Renan Silva", 4, 8.5, True]
 print(f"Nome: {aluno[0]}")
 print(f"Faltas: {aluno[1]}")
-print(F"Média: " {aluno[2]})
+print(F"Média:  {aluno[2]}")
 if aluno[3] == True: 
     print(f"Aprovado: Sim")
 else:
