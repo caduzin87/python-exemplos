b88b1f1cdba74940d885dfe6e69c1fceb39a2806
caduzin87 <carlos.bafonifacincani@gmail.com>
2024-05-23 10:11:39 -0300

    listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=['Carlos', ' Viktor', 'Lucas', 'Costa', 'Marcos', 'Carlos']
+print(f"Lista de alunos: {alunos}")
+# Método remove()
+alunos.remove('Carlos')
+print(f"Lista de alunos: {alunos}")
+# Método pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos[3]
+print(f'Lista de alunos: {alunos}')
+# Método clear()
+alunos.clear()
+print(f"Lista de alunos: {alunos}")
